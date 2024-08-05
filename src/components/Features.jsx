dcd67652b330136f618f10d4689070e2609442fb
bar47ney <48147719+bar47ney@users.noreply.github.com>
2024-08-05 18:10:54 +0300

    update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,33 @@
 import { useGSAP } from "@gsap/react";
 import React, { useRef } from "react";
 import { animateWithGsap } from "../utils/animation";
-import { exploreVideo } from "../utils";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import gsap from "gsap";
 
 const Features = () => {
   useGSAP(() => {
+    gsap.to("#exploreVideo", {
+      scrollTrigger: {
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: "-10% bottom",
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      },
+    });
     animateWithGsap("#features_title", { y: 0, opacity: 1 });
+    animateWithGsap(
+      ".g_grow",
+      { scale: 1, opacity: 1, ease: "power1" },
+      { scrub: 5.5 }
+    );
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
+    });
   }, []);
 
   const videoRef = useRef();
@@ -38,8 +60,52 @@ const Features = () => {
                 autoPlay
                 ref={videoRef}
               >
-                <source src={exploreVideo} type="video/mp4"/>
+                <source src={exploreVideo} type="video/mp4" />
               </video>
+            </div>
+
+            <div className="flex flex-col w-full relative">
+              <div className="feature-video-container">
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore1Img}
+                    alt="titanim 2"
+                    className="feature-video g_grow"
+                  />
+                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore2Img}
+                    alt="titanim 2"
+                    className="feature-video g_grow"
+                  />
+                </div>
+              </div>
+
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 Pro is{" "}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design
+                    </span>
+                    , using the same alloy that spacecrafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, making these our{" "}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
